c68aeb850cfb161d94a09161a019f2cafa911ed6
utknit05 <utkarsh.saxena950@gmail.com>
2020-02-29 17:00:28 +0530

    redux-store

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import createStore from './store';
 
-const App = () => <div>Hello React - TS</div>
+const App = () => {
+    return (
+        <Provider store={createStore()}>
+            <div>Hello</div>
+        </Provider>
+    )
+}
 
 export default App;
